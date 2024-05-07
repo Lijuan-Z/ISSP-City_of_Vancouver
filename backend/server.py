ca58658659fbdf7086e83c854b4f6eeca7e26dca
kevin ng <ong8@my.bcit.ca>
2024-05-07 16:37:27 -0700

    (Kevin) add handling config data, logging and basic error handling

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, abort
 from flask_cors import CORS
 import pandas as pd
 import io
+import configparser
+
+# config file for information management
+config = configparser.ConfigParser()
+config.read('development.ini')
 
 app = Flask(__name__)
 CORS(app)
@@ -23,15 +28,31 @@ def generate_response(query):
 
     return response
 
+# return 404 Not found for non-exist route
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+# return 500 When any there are server errors
+@app.errorhandler(500)
+def internal_error(error):
+    app.logger.error(f"server encounter error ${error.name} and returning status code 500")
+    return render_template('500.html'), 500
+
 
 @app.route("/search")
 def search():
+
     query = request.args.get('q')  # Get the search query parameter
-    if query:
+    app.logger.info(f"/search received a request of ${query}")
+    if query is not None:
         return generate_response(query)
     else:
-        return "Hello team!"
-    
+       app.logger.error(f"/search receive an empty query and returning status code 404")
+       abort(404)
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=config.get('server', 'debug'), port=config.get('server', 'port'))
