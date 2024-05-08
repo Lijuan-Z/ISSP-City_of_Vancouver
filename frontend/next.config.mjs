f5e6acc94e5307316a7316eb4b5412a533d7b03b
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-05-07 17:46:09 -0700

    fix port number

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/:path*',
-                destination: 'http://127.0.0.1:5000/:path*'
+                destination: 'http://127.0.0.1:8000/:path*'
             }
         ]
     }
