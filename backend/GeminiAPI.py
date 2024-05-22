a676602178997439ae96c232f1686c22c0a4fa2a
Vitor Guara <vrosaguara@my.bcit.ca>
2024-05-21 17:12:24 -0700

    Testing AI obj 2 format

--- backend/GeminiAPI.py
+++ backend/GeminiAPI.py
@@ -1,5 +1,7 @@
+import ast
 import json
 import time
+import re
 
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
@@ -37,7 +39,10 @@ class GeminiAPI():
         doc_count = 0
 
         for key, value in data.items():
-            file_updated = URL_info[key]['file_updated']
+            try:
+                file_updated = URL_info[key]['file_updated']
+            except KeyError:
+                file_updated = False
             if not file_updated:
                 doc_count += 1
                 new_dict = {}
@@ -72,18 +77,22 @@ class GeminiAPI():
         return data
 
     def get_amendment_and_rationale(self, search_results, prompt):
-        system_definition = ("You are receiving some pieces of information in dictionary format"
-                             "The format is {<reference number>: <content>}."
-                             "You will also receive a prompt"
-                             "Based on the prompt you will return an amendment and rationale for each content."
-                             "If no amendment is necessary, return 'no amendment' in both fields."
-                             "Return is in dictionary format: "
-                             "{'Amendment<reference number>': Amendement suggestion, 'Rationale<reference number>': Rationale Suggestion}."
-                             "Indicate removal by surrounding it with <remove></remove> symbol."
-                             "Example: This is a text that contains <remove> part to be removed</remove>"
-                             "Indicate replaced text by surrounding it with <add></add> symbol"
-                             "Example: This is a text that contains <remove> part to be removed</remove> <add> part corrected </add>"
-                             "Ensure that the resulting amendment text is still coherent after removing and adding parts"
+        system_definition = ('You are receiving some pieces of information in dictionary format'
+                             'The format is {<reference number>: <content>}.'
+                             'You will also receive a prompt'
+                             'Based on the prompt you will return an amendment and rationale for each content.'
+                             'Follow the instructions of the prompt, for example'
+                             '"Change mentions of window shape and size to "see window-by-law"". '
+                             '-> Means change only if there is mention of shape and size of window.'
+                             'If no amendment is necessary, return "no amendment" in both fields.'
+                             'Return is in dictionary format: '
+                             '{"Amendment<reference number>": "Amendement suggestion", "Rationale<reference number>": "Rationale Suggestion"}.'
+                             'Ensure that every key and value end with a double quote and no other double quotes exist in the dictionary.'
+                             'Indicate removal by surrounding it with <remove></remove> symbol.'
+                             'Example: This is a text that contains <remove> part to be removed</remove>'
+                             'Indicate replaced text by surrounding it with <add></add> symbol'
+                             'Example: This is a text that contains <remove> part to be removed</remove> <add> part corrected </add>'
+                             'Ensure that the resulting amendment text is still coherent after removing and adding parts'
                              )
         genai.configure(api_key=GOOGLE_API_KEY)
         model = genai.GenerativeModel(self.model,
@@ -113,17 +122,70 @@ class GeminiAPI():
         print(f"Total instances found: {total_count}")
 
         # return instances_search
-
-
-        for key, instance_group in instances_search:
+        timeout = 60
+        instance_count = 0
+        for key, instance_group in instances_search.items():
             for instance in instance_group:
                 query = f"Prompt: {prompt}\n References :{instance}"
 
                 response = model.generate_content(
                     contents=query
                 )
+                response = self.clear_json_response(response.text)
+                print("\n\n")
+                print(response)
+                print(key)
+                data = self.add_response_to_search_results(data, response, key)
+                instance_count += 1
+                if instance_count % 10 == 0:
+                    print(f"time out number: {instance_count/10}")
+                    time.sleep(timeout)
+
+        return data
+
+    def add_response_to_search_results(self, search_results,response, file_name):
+        #turn the response into an actual dictionary
+        # response = json.loads(response)
+        for key, value in response.items():
+            if key.startswith("Amendment"):
+                index = int(key.split('ment')[1])
+                search_results[file_name][index]['Amendment'] = value
+                rationale_key = f"Rationale{index}"
+                search_results[file_name][index][rationale_key] = response.get(rationale_key)
+
+        return search_results
 
 
+    def clear_json_response(self, json_response):
+        print(json_response)
+        if json_response[3:7] == "json":
+            json_response = json_response[7:-3]
+        print("\n")
+        json_response = json.dumps(json_response)
+        # actual_dict = json_response
+        # pattern = r'(".*?\'\s*[,}])|(\s*[,{]\s*\'[^"\'{}]+"\s*[,}])'
+        # pattern = r'(".*?\'[,}])|(\'.*?"[,}])'
+        pattern = r',\s*\"(Amendment\d+|Rationale\d+)\":'
+        corrected_dict_str = re.sub(pattern, GeminiAPI.correct_quotes, json_response)
+        actual_dict = ast.literal_eval(corrected_dict_str)
+        print(actual_dict)
+        print(type(actual_dict))
+        if not isinstance(actual_dict, dict):
+            actual_dict = json.loads(actual_dict)
+            # actual_dict = json.loads(actual_dict)
+        print(type(actual_dict))
+
+        return actual_dict
+
+    @staticmethod
+    def correct_quotes(match):
+        # text = match.group(0)
+        # if text.startswith('"') and text.endswith("'") and (text[-2] in ',}'):
+        #     return text[:-1] + '"'
+        # elif text.startswith("'") and text.endswith('"') and (text[0] in ',{'):
+        #     return '"' + text[1:]
+        # return text
+        return '", "' + match.group(1) + '":'
 
 
 
@@ -131,7 +193,7 @@ if __name__ == "__main__":
     gemini = GeminiAPI()
     with open("reduced_output.json", "r") as file:
         data = json.load(file)
-    prompt = "If there is any mention to parking space on parking spots, replace with 'See parking by-law'"
+    prompt = "If there is any mention to parking space on parking spots only, replace with 'See parking by-law'"
     test = gemini.get_amendment_and_rationale(data, prompt)
     with open("instances.json", "w") as file:
         json.dump(test, file)
--- backend/process_to_JSON.py
+++ backend/process_to_JSON.py
@@ -18,41 +18,61 @@ class ProcessToJSON:
             self.current_data = {}
 
 
-    def read_PDFs(self, URL_info, image_included=False, file_list_inc_image=[]):
+    def read_PDFs(self, URL_info, image_included=False):
+
+        URL_info = self.adjust_URL_info(URL_info)
         global process_update
         nested_metadata_dict = self.current_data
         files_to_update = self.count_total_files(URL_info)
         file_update_counter = 0
+        startTime = time.time()
         for root, dirs, files in os.walk(self.folder_path):
             for file_name in files:
-                file_updated = URL_info[file_name[:-4]]['file_updated']
+                try:
+                    file_updated = URL_info[file_name[:-4]]['file_updated']
+                except KeyError:
+                    file_updated = False
                 if file_name.endswith('.pdf') and not file_updated:
                     file_path = os.path.join(root, file_name)
                     try:
                         with open(file_path, mode='rb') as file:
-                            process_update = f"Updating file{file_update_counter} of {files_to_update}"
+                            file_update_counter += 1
+                            process_update = f"Updating file {file_update_counter} of {files_to_update}"
+                            file_time = time.time()
+                            print(process_update)
                             reader = pypdf.PdfReader(file)
                             nested_metadata_dict[file_name] = {}
                             nested_metadata_dict[file_name]['Pages'] = {}
                             for page in reader.pages:
                                 page_num = str(page.page_number + 1)
-                                if not image_included:
+                                if not image_included or not page.images:
                                     page_text = page.extract_text()
                                 elif image_included and page.images:
                                     page_text = self.get_image_text(page)
-                                else:
-                                    page_text = ""
                                 nested_metadata_dict[file_name]['Pages'][page_num] = page_text
+                            print(f"Time taken for file {file_name}: {time.time() - file_time}")
                     except Exception as e:
                         print(f"{file_name} could not be updated. Error: {e}")
+
+
+        #saving a file just in case
+        with open('processed.json', 'w') as json_file:
+            json.dump(nested_metadata_dict, json_file, indent=4)
 
 
         #Get info from the URL info
         nested_metadata_dict = self.add_file_info_to_JSON(nested_metadata_dict, URL_info)
 
+        print(time.time() - startTime)
+
+        #again just in case
+        #saving a file just in case
+        with open('processed.json', 'w') as json_file:
+            json.dump(nested_metadata_dict, json_file, indent=4)
+
         #Add the AI titles using gemini
-        gemAI = GeminiAPI(nested_metadata_dict)
-        nested_metadata_dict = gemAI.find_title()
+        gemAI = GeminiAPI()
+        nested_metadata_dict = gemAI.find_title(URL_info, nested_metadata_dict)
 
         return nested_metadata_dict
 
@@ -60,19 +80,19 @@ class ProcessToJSON:
         files_to_update = 0
         for root, dirs, files in os.walk(self.folder_path):
             for file_name in files:
-                file_updated = URL_info[file_name[:-4]]['file_updated']
+                try:
+                    file_updated = URL_info[file_name[:-4]]['file_updated']
+                except KeyError:
+                    file_updated = False
                 if file_name.endswith('.pdf') and not file_updated:
                     files_to_update += 1
 
         return files_to_update
 
     def add_file_info_to_JSON(self, nested_metadata_dict, URL_info):
+
         for key in nested_metadata_dict.keys():
-            dict_info = {}
-            for value in URL_info:
-                if key[:-4] == list(value.keys())[0]:
-                    dict_info = value
-                    break
+            dict_info = URL_info
             try:
                 nested_metadata_dict[key]['Title'] = dict_info[key[:-4]]['title']
                 nested_metadata_dict[key]['Link'] = dict_info[key[:-4]]['url']
@@ -85,6 +105,14 @@ class ProcessToJSON:
 
         return nested_metadata_dict
 
+    def adjust_URL_info(self, URL_info):
+
+        #remove the list
+        new_URL_info = {}
+        for item in URL_info:
+            new_URL_info.update(item)
+
+        return new_URL_info
 
     def get_image_text(self, page):
         image_reader = easyocr.Reader(['en'])
@@ -97,24 +125,35 @@ class ProcessToJSON:
                 image_info += f"IMAGE: {image_text}"
         except:
             print("Could not read image")
-        page_text = page.extract_text()
-        page_text += f" {image_info}"
+
+        page_text = f"{page.extract_text()} {image_info}"
         return page_text
 
 
 if __name__ == '__main__':
     # folder_path = 'test_pdfs'
-    folder_path = '../downloaded_pdfs'
+    folder_path = 'downloaded_pdfs'
 
     # Search for PDFs containing search term
     processor = ProcessToJSON(folder_path)
-    URL_info = []
     with open('doc_type.json') as json_file:
         data = json.load(json_file)
 
+
     start = time.time()
-    dict_info = processor.read_PDFs(image_included=False, URL_info=data)
+    dict_info = processor.read_PDFs(image_included=True, URL_info=data)
     print(time.time() - start)
 
-    with open('processed.json', 'w') as json_file:
+
+    # # part Below only if AI title didn't run
+    # with open("processed.json", "r") as processed_file:
+    #     processed_data = json.load(processed_file)
+    #
+    # data = processor.adjust_URL_info(data)
+    #
+    # gemAI = GeminiAPI()
+    # dict_info = gemAI.find_title(data, processed_data)
+    # # end of AI title only
+
+    with open('processed_final.json', 'w') as json_file:
         json.dump(dict_info, json_file, indent=4)
