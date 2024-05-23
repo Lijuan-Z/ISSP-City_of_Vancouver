e0e2b9f35b037d50e7d042ef82ba3fa68ddc54ba
kevin ng <ong8@my.bcit.ca>
2024-05-23 15:52:40 -0700

    (kevin): 1. create error excel output from /search 2. create /data/03 endpoint 3. update /search/o3

--- backend/server.py
+++ backend/server.py
@@ -30,16 +30,22 @@ update_status = False
 # Generate Excel file based on the query
 def generate_response(query, files):
     start_time = time.time()
+    excel_file_path = "output.xlsx"
     # output_str = searching_endpoint(query)
     # output_dict = search_files(files, json_path="processed_final.json", search_terms=query)
-    output_dict = search_files(files, json_path=config.get('server', 'processed_json_file'), search_terms=query)
-    excel_file_path = "output.xlsx"
-    OutputHandler.create_excel_file(output_dict, output_file=excel_file_path)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    # Print the elapsed time
-    print("Elapsed time:", elapsed_time, "seconds")
+    try:
+        output_dict = search_files(files, json_path=config.get('server', 'processed_json_file'), search_terms=query)
+        OutputHandler.create_excel_file(output_dict, output_file=excel_file_path)
+    except Exception as e:
+        msg = f"There is an error when searching from AI. code: {e}"
+        print(msg)
+        output_excel = pd.DataFrame({'Error': {"message": msg}})
+        output_excel.to_excel(excel_file_path)
+    finally:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        # Print the elapsed time
+        print("Elapsed time:", elapsed_time, "seconds")
 
     with open(excel_file_path, "rb") as file:
         file_data = file.read()
@@ -227,10 +233,18 @@ def search_o3():
             print(file_list)
             app.logger.info(f'/search/o3: is going to search {len(file_list)} files')
             
-            obj3_data = enter_obj3(file_list)   # temp gen output objective 3
-            response = make_response(obj3_data)
-            response.headers["Content-Disposition"] = f"attachment; filename=output_o3.xlsx"
-            response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            # obj3_data = enter_obj3(file_list)   # temp gen output objective 3
+            # response = make_response(obj3_data)
+            # response.headers["Content-Disposition"] = f"attachment; filename=output_o3.xlsx"
+            # response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+            output = f"requesting AI to generate report for {len(file_list)} files"
+            app.logger.info(f"/search/o3: is returning a response")
+            response = app.response_class(
+                response=json.dumps({"data": output}),
+                status=200,
+                mimetype='application/json'
+            )
 
             return response
         else:
@@ -266,6 +280,30 @@ def data():
         output = read_data_type_file()
 
         app.logger.info(f"/data: return {len(output)} files response")
+        response = app.response_class(
+            response=json.dumps({"data": output}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    except Exception as e:
+        app.logger.error(f"/data: Error in loading file - {e}")
+        abort(500)
+
+@app.route("/data/o3")
+def data_o3():
+    app.logger.info(f"/data/o3: received a request")
+    try:
+        output_msg = "Asking AI to generate file - step (2/10) of zoning-by-law-district-schedule-fc-1.pdf"
+        output_msg = "Asking AI to generate file - step (6/10) of zoning-by-law-district-schedule-r1-1.pdf"
+        # output_msg = "finished excel creation output_03.xlsx in folder /LZR"
+
+        o3_file_status = False
+
+        output = {"data": output_msg,
+                  "is_created": o3_file_status}
+
+        app.logger.info(f"/data: returning {len(output)} files response")
         response = app.response_class(
             response=json.dumps({"data": output}),
             status=200,
